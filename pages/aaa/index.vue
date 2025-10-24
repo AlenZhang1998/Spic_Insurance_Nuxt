757ca3609b4565aee62bc66be0b320231d884c73
alenzhang <alenzhang@yflife.com>
2025-10-24 10:38:03 +0800

    upd: delete useless file

--- pages/aaa/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="aaa">
-    aaa页面
-    <el-button type="primary">点击我</el-button>
-  </div>
-</template>
--- pages/home/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="home">
-    home页面
-  </div>
-</template>
