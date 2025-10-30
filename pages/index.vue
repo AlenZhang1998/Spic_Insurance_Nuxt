37bac35efb897fb5ab79579b70adc8c151324ecb
Alen Zhang <1464253423@qq.com>
2025-10-30 14:11:04 +0800

    upd: 首页-SEO标题

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,16 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import HomeBannerCarousel from '@/components/home/HomeBannerCarousel.vue'
-import HomeNewsCenter from '@/components/home/HomeNewsCenter.vue'
-import HomeRiskBanner from '@/components/home/HomeRiskBanner.vue'
-import HomeBusinessOverview from '@/components/home/HomeBusinessOverview.vue'
-import HomeEnterpriseImage from '@/components/home/HomeEnterpriseImage.vue'
+<script setup>
+import HomeBannerCarousel from '@/components/home/HomeBannerCarousel.vue';
+import HomeNewsCenter from '@/components/home/HomeNewsCenter.vue';
+import HomeRiskBanner from '@/components/home/HomeRiskBanner.vue';
+import HomeBusinessOverview from '@/components/home/HomeBusinessOverview.vue';
+import HomeEnterpriseImage from '@/components/home/HomeEnterpriseImage.vue';
+
+useHead(() => ({
+  title: '国家电投集团保险经纪公司',
+}));
 </script>
 
 <style scoped lang="scss">
